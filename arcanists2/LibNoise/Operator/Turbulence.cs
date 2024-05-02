9660a9332873f514f92acacfaf8dd00b9d82babc
Kattoor <jcatthoor@inimco.com>
2024-05-02 16:04:12 +0200

    /

--- arcanists2/LibNoise/Operator/Turbulence.cs
+++ arcanists2/LibNoise/Operator/Turbulence.cs
@@ -1,8 +1,4 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: LibNoise.Operator.Turbulence
-// Assembly: Assembly-CSharp, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: DA7163A9-CD4F-457E-9379-B1755B6F3B01
-// Assembly location: C:\Users\jaspe\Downloads\Arcanists6.8\Arcanists 2_Data\Managed\Assembly-CSharp.dll
+﻿
 
 using LibNoise.Generator;
 
--- arcanists2/UnityEngine/UI/Extensions/CableCurve.cs
+++ arcanists2/UnityEngine/UI/Extensions/CableCurve.cs
@@ -1,8 +1,4 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: UnityEngine.UI.Extensions.CableCurve
-// Assembly: Assembly-CSharp, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: DA7163A9-CD4F-457E-9379-B1755B6F3B01
-// Assembly location: C:\Users\jaspe\Downloads\Arcanists6.8\Arcanists 2_Data\Managed\Assembly-CSharp.dll
+﻿
 
 using System;
 
