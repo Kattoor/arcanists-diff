53e0e4838ad090246cc90c588db68b24049b8b51
Kattoor <jcatthoor@inimco.com>
2024-05-02 16:05:51 +0200

    /

--- arcanists2/SevenZip/Compression/LZMA/Decoder.cs
+++ arcanists2/SevenZip/Compression/LZMA/Decoder.cs
@@ -1,8 +1,4 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: SevenZip.Compression.LZMA.Decoder
-// Assembly: Assembly-CSharp, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: D266BEE2-E7E9-4299-9752-8BB93E4AAF85
-// Assembly location: C:\Users\jaspe\Downloads\Arcanists6.9\Arcanists 2_Data\Managed\Assembly-CSharp.dll
+﻿
 
 using SevenZip.Compression.LZ;
 using SevenZip.Compression.RangeCoder;
